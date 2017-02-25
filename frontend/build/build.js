1fbb541389ad3022597baf8249c638d6dbe9153e
Kazumune Katagiri <kkaz123@gmail.com>
2017-02-25 09:32:52 +0900

    watch modeの時だけsourceMapを作成するように変更

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -9,10 +9,12 @@ var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
+webpackConfig.devtool = false;
 if(process.argv.length >= 3) {
   for(var i = 2; i< process.argv.length; i++ ) {
     if(process.argv[i] === "watch") {
       webpackConfig.watch = true;
+      webpackConfig.devtool = '#source-map';
     }
   }
 }
